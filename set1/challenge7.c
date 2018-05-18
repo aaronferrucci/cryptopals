8b2a27ad84824509a571e4852215fb49e3809909
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2018-05-17 21:57:19 -0700

    Moving closer to actually decrypting.

--- set1/challenge7.c
+++ set1/challenge7.c
@@ -7,39 +7,48 @@
 #include <openssl/x509.h>
 
 #include "c7_data.h"
+#include "utils.h"
 
 typedef struct INMEM_FILE {
   unsigned char *data;
-  size_t index;
-  size_t length;
+  size_t cur_index;
+  size_t size;
 } INMEM_FILE;
 
 size_t inmem_fread(unsigned char *buf, size_t num_bytes, INMEM_FILE *p)
 {
-  if (p->index >= length)
-    return 0;
+  if (p->cur_index >= p->size)
+    return 0; // no more data left to read
+
   // copy up to num_bytes bytes into buf
-  size_t remaining = p->length - p->index;
+  size_t remaining = p->size - p->cur_index;
   size_t n =  remaining > num_bytes ? num_bytes : remaining;
 
-  memcpy(buf, p->data[index], n);
-  index += n;
+  memcpy(buf, &p->data[p->cur_index], n);
+  p->cur_index += n;
 
   return n;
 }
 
 size_t inmem_fwrite(unsigned char *writedata, size_t num_bytes, INMEM_FILE *p)
 {
-  if (p->index >= length)
-    return 0;
-  size_t remaining = p->length - p->index;
+  if (p->cur_index >= p->size)
+    return 0; // no more space left to write to
+
+  // copy at most num_bytes bytes from writedata
+  size_t remaining = p->size - p->cur_index;
   size_t n =  remaining > num_bytes ? num_bytes : remaining;
-  memcpy(p->data[index], writedata, n);
-  index += n;
+
+  memcpy(&p->data[p->cur_index], writedata, n);
+  p->cur_index += n;
+
   return n;
 }
 
-int decrypt(INMEM_FILE *in, size_t inlen, unsigned char *out)
+// possible useful notes on ssl usage here:
+// http://theshybulb.com/2015/10/10/use-openssl-c-library.html
+//
+int decrypt(INMEM_FILE *in, INMEM_FILE *out)
 {
   const int do_encrypt = 0; // decrypt
 
@@ -52,20 +61,20 @@ int decrypt(INMEM_FILE *in, size_t inlen, unsigned char *out)
   * another source.
   */
   unsigned char key[] = "YELLOW SUBMARINE";
-  // unsigned char iv[] = "1234567887654321";
+  unsigned char iv[] = "1234567887654321";
 
   /* Don't set key or IV right away; we want to check lengths */
   ctx = EVP_CIPHER_CTX_new();
-  EVP_CipherInit_ex(&ctx, EVP_aes_128_ebc(), NULL, NULL, NULL,
+  EVP_CipherInit_ex(ctx, EVP_aes_128_ecb(), NULL, NULL, NULL,
                     do_encrypt);
   OPENSSL_assert(EVP_CIPHER_CTX_key_length(ctx) == 16);
   // OPENSSL_assert(EVP_CIPHER_CTX_iv_length(ctx) == 16);
 
   /* Now we can set key and IV */
-  EVP_CipherInit_ex(ctx, NULL, NULL, key, NULL, do_encrypt);
+  EVP_CipherInit_ex(ctx, NULL, NULL, key, iv, do_encrypt);
 
   for (;;) {
-    inlen = fread(inbuf, 1, 1024, in);
+    inlen = inmem_fread(inbuf, 1024, in);
     if (inlen <= 0)
         break;
     if (!EVP_CipherUpdate(ctx, outbuf, &outlen, inbuf, inlen)) {
@@ -73,14 +82,14 @@ int decrypt(INMEM_FILE *in, size_t inlen, unsigned char *out)
         EVP_CIPHER_CTX_free(ctx);
         return 0;
     }
-    fwrite(outbuf, 1, outlen, out);
+    inmem_fwrite(outbuf, outlen, out);
   }
   if (!EVP_CipherFinal_ex(ctx, outbuf, &outlen)) {
     /* Error */
     EVP_CIPHER_CTX_free(ctx);
     return 0;
   }
-  fwrite(outbuf, 1, outlen, out);
+  inmem_fwrite(outbuf, outlen, out);
 
   EVP_CIPHER_CTX_free(ctx);
   return 1;
@@ -88,7 +97,14 @@ int decrypt(INMEM_FILE *in, size_t inlen, unsigned char *out)
 
 int main(void)
 {
+  unsigned char *raw;
+  size_t raw_len;
   printf("%lu bytes of base64-encoded data\n", strlen(base64_data));
+
+  raw = base64_decode(base64_data, &raw_len);
+  printf("%lu bytes of decoded data\n", raw_len);
+
+  free(raw); raw = NULL;
   return 0;
 }
 
--- set1/utils.h
+++ set1/utils.h
@@ -5,7 +5,11 @@ unsigned char hex_convert(char *s);
 void decode_hex_string(char *s, unsigned char *bytes, int byte_count);
 size_t count_bits(unsigned char c);
 size_t hamming(char *s1, char *s2, size_t len);
+
+// allocates memory, decodes input data into it, returns a pointer to 
+// decoded data. Caller calls free(). len is set to the output data length.
 unsigned char *base64_decode(unsigned char *base64_data, size_t *len);
+
 float score_etaoin(unsigned char *data, int start, int stride, int len);
 unsigned char max_xor_key(unsigned char *data, int start, int stride, int len);
 void xor_decode(unsigned char *data, unsigned char key, int len);
