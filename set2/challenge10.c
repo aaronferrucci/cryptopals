7b818b62273df3b8948e86b26e7ac3669f01cead
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2018-06-18 21:02:40 -0700

    Progress on set2 challenge10.

--- set2/challenge10.c
+++ set2/challenge10.c
@@ -1,6 +1,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 #include <openssl/evp.h>
 #include <openssl/ssl.h>
@@ -8,51 +9,57 @@
 #include <openssl/x509.h>
 
 #include "c10_data.h"
-#include "inmem_file.h"
+#include "utils.h"
 
-int encrypt(INMEM_FILE *in, INMEM_FILE *out, unsigned char *key, unsigned char *iv)
+int block_crypt(unsigned char *input, unsigned char *output, unsigned char *iv, unsigned char *key, int do_encrypt)
 {
-  const int do_encrypt = 1; // encrypt
-
-  /* Allow enough space in output buffer for additional block */
-  unsigned char inbuf[1024], outbuf[1024 + EVP_MAX_BLOCK_LENGTH];
-  int inlen, outlen;
   EVP_CIPHER_CTX *ctx;
-  /*
-  * Bogus key and IV: we'd normally set these from
-  * another source.
-  */
-
-  /* Don't set key or IV right away; we want to check lengths */
+  int outlen;
   ctx = EVP_CIPHER_CTX_new();
-  EVP_CipherInit_ex(ctx, EVP_aes_128_ecb(), NULL, NULL, NULL,
-                    do_encrypt);
-  OPENSSL_assert(EVP_CIPHER_CTX_key_length(ctx) == 16);
-  // OPENSSL_assert(EVP_CIPHER_CTX_iv_length(ctx) == 16);
-
-  /* Now we can set key and IV */
-  EVP_CipherInit_ex(ctx, NULL, NULL, key, iv, do_encrypt);
-
-  for (;;) {
-    inlen = inmem_fread(inbuf, 1024, in);
-    if (inlen <= 0)
-        break;
-    if (!EVP_CipherUpdate(ctx, outbuf, &outlen, inbuf, inlen)) {
-        /* Error */
-        EVP_CIPHER_CTX_free(ctx);
-        return 0;
-    }
-    inmem_fwrite(outbuf, outlen, out);
-  }
-  if (!EVP_CipherFinal_ex(ctx, outbuf, &outlen)) {
+  EVP_CipherInit_ex(ctx, EVP_aes_128_ecb(), NULL, key, NULL, do_encrypt);
+  if (!EVP_CipherUpdate(ctx, output, &outlen, input, 16)) {
     /* Error */
     EVP_CIPHER_CTX_free(ctx);
     return 0;
   }
-  inmem_fwrite(outbuf, outlen, out);
-
   EVP_CIPHER_CTX_free(ctx);
   return 1;
+}
+
+void print16(unsigned char *data)
+{
+  for (int i = 0; i < 16; ++i) {
+    printf("%02X ", data[i]);
+  }
+  printf("  ");
+  for (int i = 0; i < 16; ++i) {
+    printf("%c", isprint(data[i]) ? data[i] : '.');
+  }
+  printf("\n");
+}
+
+void main(void)
+{
+  unsigned char key[] = "YELLOW SUBMARINE";
+  unsigned char iv[16] = {'\xAA',};
+  unsigned char plain[] = "abcdefghijklmnop"; 
+  unsigned char encrypted[17] = {'\0',};
+  unsigned char decrypted[17] = {'\0',};
+
+  print16(key);
+
+/*
+  block_crypt(plain, encrypted, iv, key, 1);
+  print16(encrypted);
+
+  block_crypt(encrypted, decrypted, iv, key, 0);
+  print16(decrypted);
+*/
+
+  size_t data_size;
+  unsigned char *data = base64_decode(base64_data, &data_size);
+  block_crypt(data, decrypted, iv, key, 0);
+  print16(decrypted);
 }
 
 
