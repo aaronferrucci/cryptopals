7e36428914e3383baf885787fb1aeb8b1433ccdf
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2018-07-11 21:46:06 -0700

    Progress on challenge13 utilities. No idea how to solve yet.

--- set2/challenge13.c
+++ set2/challenge13.c
@@ -8,34 +8,132 @@
 #include "cbc_ecb128.h"
 #include "utils.h"
 
+static unsigned char key[16] = {'\0',};
+
+// will malloc space for the output. Caller is responsible to free().
+unsigned char *pad(size_t len, size_t block_size)
+{
+  size_t pad_size = block_size - (len % block_size);
+
+  unsigned char *pad = malloc((1 + pad_size) * sizeof(unsigned char));
+  unsigned char *pret = pad;
+  for (int i = 0; i < pad_size; ++i)
+    *pad++ = (unsigned char)pad_size;
+  *pad++ = '\0';
+  return pret;
+}
+
+void init()
+{
+  // one-time setup
+  while (!key[0]) {
+    random16(key);
+  }
+}
+
+// aes-128-ecb(input, key)
+// mallocs the return string; caller must free
+unsigned char *ecb_encrypt(unsigned char *input, size_t in_len, size_t *out_len)
+{
+  unsigned char *padding = pad(in_len, 16);
+  size_t len = in_len + strlen(padding);
+  unsigned char *plaintext = malloc(len * sizeof(unsigned char));
+  unsigned char *output = malloc(len * sizeof(unsigned char));
+  *out_len = len;
+
+  for (int i = 0; i < in_len; ++i)
+    plaintext[i] = input[i];
+  for (int i = 0; i < strlen(padding); ++i)
+    plaintext[i + in_len] = padding[i];
+  free(padding);
+
+  printf("padded plaintext:\n");
+  printX(plaintext, len);
+  printf("\n");
+  ecb128_encrypt(plaintext, output, len, key);
+  free(plaintext);
+  return output;
+}
+
+unsigned char *ecb_decrypt(unsigned char *input, size_t crypt_len)
+{
+  unsigned char *output = malloc(crypt_len * sizeof(unsigned char));
+
+  ecb128_decrypt(input, output, crypt_len, key);
+  unsigned char last_pad = output[crypt_len - 1];
+  assert(last_pad > 0 && last_pad <= 16);
+  unsigned char *pfirst_pad = output + crypt_len - last_pad;
+  *pfirst_pad = '\0';
+  return output;
+}
+
+void deinit(void)
+{
+}
+
 typedef struct t_profile {
   unsigned char *email;
   unsigned char uid;
   unsigned char *role;
 } t_profile;
 
+t_profile *new_profile(unsigned char *email, int uid, const unsigned char *role)
+{
+  t_profile *prof = (t_profile*)malloc(sizeof(t_profile));
+  prof->email = (unsigned char*)malloc((strlen(email) + 1) * sizeof(unsigned char));
+  unsigned char *p = prof->email;
+  // copy new_email into prof->email, omitting illegal bytes
+  while (*email) {
+    if (*email != '=' && *email != '&')
+      *p++ = *email;
+    email++;
+  }
+  *p = '\0';
+
+  prof->uid = uid;
+  prof->role = (unsigned char*)malloc(strlen(role) + 1);
+  strcpy(prof->role, role);
+  return prof;
+}
+
+t_profile *parse(unsigned char *encoded)
+{
+  // These arrays are too big! That's ok.
+  unsigned char *email = malloc((strlen(encoded) + 1) * sizeof(unsigned char));
+  int uid;
+  unsigned char *role = malloc((strlen(encoded) + 1) * sizeof(unsigned char));
+
+  unsigned char *token = strtok(encoded, "&=");
+  unsigned char *prev_token = NULL;
+  while (token) {
+    if (prev_token) {
+      if (!strcmp(prev_token, "email")) {
+        strcpy(email, token);
+      } else if (!strcmp(prev_token, "uid")) {
+        uid = atoi(token);
+      } else if (!strcmp(prev_token, "role")) {
+        strcpy(role, token);
+      }
+    }
+    prev_token = token;
+    token = strtok(NULL, "&=");
+  }
+
+  t_profile *prof = new_profile(email, uid, role);
+  free(email);
+  free(role);
+  return prof;
+}
+
 t_profile *profile_for(unsigned char *new_email)
 {
-  static const char *default_user = "user";
+  static const char *default_role = "user";
   static unsigned char next_uid = 10;
   // Don't allow overflow.
   if (next_uid == 0)
     return NULL;
 
-  t_profile *prof = (t_profile*)malloc(sizeof(t_profile));
-  prof->email = (unsigned char*)malloc((strlen(new_email) + 1) * sizeof(unsigned char));
-  unsigned char *p = prof->email;
-  // copy new_email into prof->email, omitting illegal bytes
-  while (*new_email) {
-    if (*new_email != '=' && *new_email != '&')
-      *p++ = *new_email;
-    new_email++;
-  }
-  *p = '\0';
-
-  prof->uid = next_uid++;
-  prof->role = (unsigned char*)malloc(strlen(default_user) + 1);
-  strcpy(prof->role, default_user);
+  t_profile *prof = new_profile(new_email, next_uid++, default_role);
   return prof;
 }
 
@@ -73,10 +171,9 @@ void delete(t_profile **pprof)
 
 int main(void)
 {
+  init();
   unsigned char *emails[] = {
-    "foo@foobar.com",
-    "wexilla@prodeunt.com",
-    "test=&123@===mon&key.com",
+    "a123456@b.com",
   };
   t_profile *prof;
   unsigned char *prof_str;
@@ -88,10 +185,29 @@ int main(void)
     prof_str = to_str(prof);
     printf("%s\n\n", prof_str);
 
+    size_t out_len;
+    unsigned char *crypt = ecb_encrypt(prof_str, strlen(prof_str), &out_len);
+    printf("encrypted:\n");
+    printX(crypt, out_len);
+    printf("\n");
+
+    unsigned char *decrypt = ecb_decrypt(crypt, out_len);
+    printf("decrypted, unpadded:\n");
+    printf("%s\n\n", decrypt);
+
+    printf("parsed:\n");
+    t_profile *prof2 = parse(decrypt);
+    print(prof2);
+    printf("\n");
+
     delete(&prof);
+    delete(&prof2);
     free(prof_str);
+    free(crypt);
+    free(decrypt);
   }
 
+  deinit();
   return 0;
 }
 
